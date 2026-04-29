0997c2493f23235bdbd163ae918b274cd9a5812d
MerryDreambound <italiyankin0@gmail.com>
2026-04-29 16:42:51 +0100

    Implement new algorithm, and configs

--- src/main/java/com/merrydreambound/createbreaking/config/CreateBreakingConfig.java
+++ src/main/java/com/merrydreambound/createbreaking/config/CreateBreakingConfig.java
@@ -1,7 +1,6 @@
 package com.merrydreambound.createbreaking.config;
 
 import com.merrydreambound.createbreaking.CreateBreaking;
-import me.fzzyhmstrs.fzzy_config.api.SaveType;
 import me.fzzyhmstrs.fzzy_config.config.Config;
 import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedDouble;
 import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedNumber;
@@ -12,21 +11,8 @@ public class CreateBreakingConfig extends Config {
     public CreateBreakingConfig() {
         super(ResourceLocation.fromNamespaceAndPath(CreateBreaking.MODID,"config"));
     }
-
-//    @RequiresAction(action = Action.RESTART)
-    public ValidatedDouble Weightless_TriggerSpeed = new ValidatedDouble(0.125, 256, 0.125, ValidatedNumber.WidgetType.TEXTBOX); //this field has defined validation, error correction, and will restrict user inputs to doubles between 0 and 10.
-//    @RequiresAction(action = Action.RESTART)
-    public ValidatedDouble Super_Light_TriggerSpeed = new ValidatedDouble(0.25, 256, 0.125, ValidatedNumber.WidgetType.TEXTBOX); //this field has defined validation, error correction, and will restrict user inputs to doubles between 0 and 10.
-//    @RequiresAction(action = Action.RESTART)
-    public ValidatedDouble Light_TriggerSpeed = new ValidatedDouble(0.5, 256, 0.125, ValidatedNumber.WidgetType.TEXTBOX); //this field has defined validation, error correction, and will restrict user inputs to doubles between 0 and 10.
-//    @RequiresAction(action = Action.RESTART)
-    public ValidatedDouble Default_TriggerSpeed = new ValidatedDouble(1, 256, 0.125, ValidatedNumber.WidgetType.TEXTBOX); //this field has defined validation, error correction, and will restrict user inputs to doubles between 0 and 10.
-//    @RequiresAction(action = Action.RESTART)
-    public ValidatedDouble Heavy_TriggerSpeed = new ValidatedDouble(2, 256, 0.125, ValidatedNumber.WidgetType.TEXTBOX); //this field has defined validation, error correction, and will restrict user inputs to doubles between 0 and 10.
-//    @RequiresAction(action = Action.RESTART)
-    public ValidatedDouble Super_Heavy_TriggerSpeed = new ValidatedDouble(4, 256, 0.125, ValidatedNumber.WidgetType.TEXTBOX); //this field has defined validation, error correction, and will restrict user inputs to doubles between 0 and 10.
-
-    public ValidatedDouble ForceCallback = new ValidatedDouble(4, 256, 0.125, ValidatedNumber.WidgetType.TEXTBOX); //this field has defined validation, error correction, and will restrict user inputs to doubles between 0 and 10.
+    public ValidatedDouble PenetrationCost = new ValidatedDouble(8, 256, 0.125, ValidatedNumber.WidgetType.TEXTBOX); //this field has defined validation, error correction, and will restrict user inputs to doubles between 0 and 10.
+    public ValidatedDouble SpeedCost = new ValidatedDouble(64, 256, 0.125, ValidatedNumber.WidgetType.TEXTBOX); //this field has defined validation, error correction, and will restrict user inputs to doubles between 0 and 10.
 
 
 //
--- src/main/java/com/merrydreambound/createbreaking/mixin/TriggerVelocityMixin.java
+++ src/main/java/com/merrydreambound/createbreaking/mixin/TriggerVelocityMixin.java
@@ -1,22 +1,18 @@
 package com.merrydreambound.createbreaking.mixin;
 
-import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.injector.ModifyReturnValue;
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
-import com.mojang.logging.LogUtils;
+import com.merrydreambound.createbreaking.CreateBreaking;
 import dev.ryanhcode.sable.Sable;
 import dev.ryanhcode.sable.api.physics.callback.BlockSubLevelCollisionCallback;
 import dev.ryanhcode.sable.api.physics.handle.RigidBodyHandle;
 import dev.ryanhcode.sable.api.sublevel.ServerSubLevelContainer;
-import dev.ryanhcode.sable.companion.math.BoundingBox3d;
-import dev.ryanhcode.sable.companion.math.BoundingBox3dc;
 import dev.ryanhcode.sable.companion.math.JOMLConversion;
-import dev.ryanhcode.sable.companion.math.Pose3d;
 import dev.ryanhcode.sable.mixinterface.block_properties.BlockStateExtension;
 import dev.ryanhcode.sable.physics.config.block_properties.PhysicsBlockPropertyTypes;
+import dev.ryanhcode.sable.physics.config.dimension_physics.DimensionPhysicsData;
 import dev.ryanhcode.sable.sublevel.ServerSubLevel;
 import dev.ryanhcode.sable.sublevel.SubLevel;
 import dev.ryanhcode.sable.sublevel.system.SubLevelPhysicsSystem;
@@ -24,11 +20,9 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
-import org.jline.utils.Log;
 import org.joml.Vector3d;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import dev.ryanhcode.sable.physics.callback.FragileBlockCallback;
 
@@ -37,9 +31,9 @@ import javax.annotation.Nullable;
 @Mixin(FragileBlockCallback.class)
 public class TriggerVelocityMixin {
 
-    @Shadow
-    @Final
-    public static FragileBlockCallback INSTANCE;
+
+    @Unique
+    private BlockPos[] hitPositionsToCheck = new BlockPos[1];
 
     private static @Nullable ServerSubLevel getServerSubLevel(final Level level, final BlockPos pos) {
         final SubLevel subLevel = Sable.HELPER.getContaining(level, pos);
@@ -65,46 +59,52 @@ public class TriggerVelocityMixin {
         }
         ServerSubLevel sublevel = getServerSubLevel(level, pos);
         if (sublevel == null){
+            hitPositionsToCheck[0] = pos;
             return new BlockSubLevelCollisionCallback.CollisionResult(JOMLConversion.ZERO, false);
+        }else {
+            if (sublevel instanceof ServerSubLevel) {
+
+                // Avoid making 1 block falls dig through the whole world
+                double minVelocity = Math.sqrt(DimensionPhysicsData.getGravity(level).length() * 2);
+                if (impactVelocity < minVelocity) {
+                    return new BlockSubLevelCollisionCallback.CollisionResult(JOMLConversion.ZERO, false);
+                }
+
+                double mass = sublevel.getMassTracker().getMass();
+                double bounciness = ((BlockStateExtension) state).sable$getProperty(PhysicsBlockPropertyTypes.RESTITUTION.get());
+
+                RigidBodyHandle handle = system.getPhysicsHandle(sublevel);
+                Vector3d currentVelocity = handle.getLinearVelocity(new Vector3d());
+                BlockPos hitBlockPos = hitPositionsToCheck[0];
+                BlockState hitBlockState = level.getBlockState(hitBlockPos);
+                double kineticEnergy = 0.5 * impactVelocity * impactVelocity * mass;
+                double hitBlockMass = ((BlockStateExtension) hitBlockState).sable$getProperty(PhysicsBlockPropertyTypes.MASS.get());
+
+                double speedCost = hitBlockMass * (1.0-bounciness) * CreateBreaking.CONFIG.SpeedCost.get();
+                double newEnergy = Math.max(0.0, kineticEnergy - speedCost);
+                double newSpeed = Math.sqrt(2.0 * newEnergy / mass);
+
+                // Prepare for angle based force
+                //Vector3d direction = new Vector3d(currentVelocity).normalize();
+                double ratio = newSpeed / currentVelocity.length();
+
+                Vector3d deltaVelocity = new Vector3d(currentVelocity).mul(ratio - 1.0);
+
+
+                handle.addLinearAndAngularVelocity(deltaVelocity, JOMLConversion.ZERO);
+
+                //Calculate penetration
+                double penetrationDepthCost = hitBlockMass * CreateBreaking.CONFIG.PenetrationCost.get();
+                if (penetrationDepthCost == 0) {
+                    penetrationDepthCost = 0.125;
+                }
+                double penetrationDepth = newEnergy / penetrationDepthCost;
+                if (penetrationDepth >= 1.0) {
+                    level.destroyBlock(hitBlockPos, true);
+                }
+            }
         }
-        if (sublevel instanceof ServerSubLevel) {
-            double mass = sublevel.getMassTracker().getMass();
-            double bounciness = ((BlockStateExtension) state).sable$getProperty(PhysicsBlockPropertyTypes.RESTITUTION.get());
 
-            RigidBodyHandle handle = system.getPhysicsHandle(sublevel);
-            Vector3d currentVelocity = handle.getLinearVelocity(new Vector3d());
-            double currentSpeed = impactVelocity;
-            BlockPos hitBlockPos = new BlockPos((int) (sublevel.logicalPose().position().x() - 0.5 + Math.clamp(currentVelocity.x(), -1.51, 1.51)),
-                    (int) (sublevel.logicalPose().position().y() + Math.clamp(currentVelocity.y(), -1.51, 1.51)),
-                    (int) (sublevel.logicalPose().position().z() + Math.clamp(currentVelocity.z(), -1.51, 1.51)));
-            BlockState hitBlockState = level.getBlockState(hitBlockPos);
-
-
-            double hitBlockMass = ((BlockStateExtension) hitBlockState).sable$getProperty(PhysicsBlockPropertyTypes.MASS.get());
-            double speedCost = hitBlockMass * hitBlockMass * (1.0 - bounciness);
-            double newSpeed = Math.max(0.0, currentSpeed - speedCost);
-            double velocityRatioLoss;
-
-            if (currentSpeed != 0){
-                velocityRatioLoss = newSpeed / currentSpeed;
-            }else{
-                velocityRatioLoss = 1;
-            }
-            Vector3d velocityLoss = currentVelocity.mul(-(1.0 - velocityRatioLoss), new Vector3d());
-            handle.addLinearAndAngularVelocity(velocityLoss, JOMLConversion.ZERO);
-
-            //Calculate penetration
-            double kineticEnergy = 0.5 * currentSpeed * currentSpeed * mass;
-            double penetrationDepthCost = hitBlockMass * hitBlockMass;
-            if (penetrationDepthCost == 0) {
-                penetrationDepthCost = 0.125;
-            }
-            double penetrationDepth = kineticEnergy / penetrationDepthCost;
-            if (penetrationDepth >= 1.0) {
-                level.destroyBlock(hitBlockPos, true);
-            }
-            return new BlockSubLevelCollisionCallback.CollisionResult(JOMLConversion.ZERO, false);
-        }
         return new BlockSubLevelCollisionCallback.CollisionResult(JOMLConversion.ZERO, false);
     }
 }
